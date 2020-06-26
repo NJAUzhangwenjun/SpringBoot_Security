95eb00b6493472776edc414ee026040086a9a65c
zhangwenjun <njauzhangwenjun@163.com>
2020-06-26 21:37:28 +0800

    初始化项目

---
+++ src/main/java/cn/zhanghub/basicserver/BasicServerApplication.java
@@ -0,0 +1,13 @@
+package cn.zhanghub.basicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/zhanghub/basicserver/BasicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhanghub.basicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
